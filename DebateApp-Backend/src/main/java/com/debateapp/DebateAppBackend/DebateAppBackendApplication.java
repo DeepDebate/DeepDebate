4e45be25523aa2d03f54a3921c7fc6a3487711c0
Kazuki Kagoshima <kazukikagoshima2@gmail.com>
2023-02-24 10:19:25 +0900

    Add files via upload

---
+++ DebateApp-Backend/src/main/java/com/debateapp/DebateAppBackend/DebateAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.debateapp.DebateAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebateAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebateAppBackendApplication.class, args);
+	}
+
+}
---
+++ DebateApp-Backend/src/test/java/com/debateapp/DebateAppBackend/DebateAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debateapp.DebateAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebateAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
